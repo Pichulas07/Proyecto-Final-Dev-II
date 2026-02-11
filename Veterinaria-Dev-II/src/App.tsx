12972217188fe9b3a9b31b697f187cb7a1cea373
Rakoww <rakoww2@gmail.com>
2026-02-10 20:47:39 -0600

    Avance Login y register

--- Veterinaria-Dev-II/src/App.tsx
+++ Veterinaria-Dev-II/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import Header from './shared/Header'
 import Footer from './shared/Footer'
 import Home from './features/home/Home'
+import Login from './features/Login/Login'
+import Register from './features/Register/Register'
 function App() {
 
   return (
@@ -10,10 +12,11 @@ function App() {
       <BrowserRouter>
         <div className='min-h-screen flex flex-col'>
           <Header />
-          <main className='flex-grow px-6 py-8'>
+          <main className="flex-grow flex items-center justify-center px-6">
           <Routes>
-            <Route path='/' element={<Home />} />
-
+            <Route path='/' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/home' element={<Home />} />
           </Routes>
           </main>
           <Footer/>
