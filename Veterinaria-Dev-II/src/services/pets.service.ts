b1716d4bcaba0c3846a424d857d1fcf3986b7b2f
Amanda Bolanos <amandabolanosu@gmail.com>
2026-02-11 18:52:55 -0600

    json-service

---
+++ Veterinaria-Dev-II/src/services/pets.service.ts
@@ -0,0 +1,21 @@
+import type { Pet } from "../models/Pet";
+
+const PETS_URL = "../../data/pets.json";
+export async function getPets(): Promise<Pet[]> {
+    const response = await fetch(PETS_URL);
+    if (!response.ok) {
+        throw new Error(
+            `No se pudo cargar movies.json (status ${response.status})`,
+        );
+    }
+
+    const data = await response.json();
+    console.log("Peliculas cargadas", data);
+    return data as Pet[];
+}
+
+export async function getPetsByName(name: string): Promise<Pet | null>{
+    const pets = await getPets();
+    const found = pets.find((p) => String(p.name) === String(name));
+    return found ?? null;
+}
